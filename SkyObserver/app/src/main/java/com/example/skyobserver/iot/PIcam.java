945e9d45296270cbe4cc62e013701b0d8898e0d1
jobchoi <sedcreta12@gmail.com>
2019-11-27 18:46:07 +0900

    onclick 이벤트 처리를 한 메서드로, ImageView Button 클릭시 PIcam Activity 실행

--- SkyObserver/app/src/main/java/com/example/skyobserver/iot/PIcam.java
+++ SkyObserver/app/src/main/java/com/example/skyobserver/iot/PIcam.java
@@ -1,14 +1,18 @@
-package com.example.skyobserver;
+package com.example.skyobserver.iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class picam extends AppCompatActivity {
+import com.example.skyobserver.R;
+
+public class PIcam extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picam);
+        Toast.makeText(this, "IoT : pi카메라 확인", Toast.LENGTH_SHORT).show();
     }
 }
